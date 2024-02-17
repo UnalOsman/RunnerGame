9dd6effeefac6ada9a395190bd87dcbdc427fa0f
Osman Unal <20215802030@ogr.akdeniz.edu.tr>
2024-02-17 17:19:05 +0300

    added bean character

--- Assets/Scripts/character.cs
+++ Assets/Scripts/character.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class character : MonoBehaviour
 {
-    // Start is called before the first frame update
+    int hiz=10;
+
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Hareket();
     }
+
+    public void Hareket()
+    {
+        transform.position =new Vector3(transform.position.x,transform.position.y,transform.position.z*hiz*Time.deltaTime);
+
+
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.position=new Vector3(transform.position.x,transform.position.y + 0.62f,transform.position.z);
+        }
+        if(Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + 0.14f, transform.position.z);
+            transform.Rotate(new Vector3(transform.rotation.x - 80f,transform.rotation.y,transform.rotation.z),1f);
+        }
+    }
+
+
 }
