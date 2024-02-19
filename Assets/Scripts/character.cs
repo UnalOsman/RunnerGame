9394f7faf666bc1f61106d898d9de32731bbcdc6
Osman Unal <20215802030@ogr.akdeniz.edu.tr>
2024-02-19 16:53:03 +0300

    added level design and character script

--- Assets/Scripts/character.cs
+++ Assets/Scripts/character.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class character : MonoBehaviour
 {
-
+    public GameObject Base1;
     Rigidbody rb;
 
     bool havadami = false;
     int hiz=3;
-
+    Vector3 konum = Vector3.zero;
 
 
     void Start()
@@ -28,33 +28,80 @@ public class character : MonoBehaviour
 
         transform.Translate(new Vector3(0,0,hiz*Time.deltaTime));
 
-
-        
-
-        if(havadami==false)
+        if (havadami == false)
         {
             if (Input.GetKeyDown(KeyCode.UpArrow))
             {
-                rb.velocity=new Vector3 (0,600f*Time.deltaTime,0);
+                rb.velocity = new Vector3(0, 500f * Time.deltaTime, 0);
             }
             if (Input.GetKeyDown(KeyCode.DownArrow))
             {
 
             }
         }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (transform.position.x < 1.55f)
+            {
+                transform.position = new Vector3(transform.position.x + 1.55f, transform.position.y, transform.position.z);
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (transform.position.x > -1.55f)
+            {
+                transform.position = new Vector3(transform.position.x - 1.55f, transform.position.y, transform.position.z);
+            }
+        }
+
+    }
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        if( other.CompareTag("floor") || other.CompareTag("ziplananEngel"))
+        {
+            havadami = false;
+        }
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("floor") || other.CompareTag("ziplananEngel"))
+        {
+            havadami = true;
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("ileri2"))
+        {
+            if(Base1.transform.position.z==33)
+            {
+                Base1.GetComponent<Transform>().transform.position = new Vector3( Base1.transform.position.x,Base1.transform.position.y, -26.82f);
+            }
+            else
+            {
+                Base1.GetComponent<Transform>().transform.position = new Vector3(Base1.transform.position.x,Base1.transform.position.y, 33f);
+            }
+        }
     }
 
     private void OnCollisionStay(Collision collision)
     {
-        if(collision.collider.CompareTag("floor"))
+        if (collision.collider.CompareTag("floor") || collision.collider.CompareTag("ziplananEngel"))
         {
-            havadami= false;
+            havadami = false;
         }
     }
 
     private void OnCollisionExit(Collision collision)
     {
-        if(collision.collider.CompareTag("floor"))
+        if (collision.collider.CompareTag("floor") || collision.collider.CompareTag("ziplananEngel"))
         {
             havadami = true;
         }
